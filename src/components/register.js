1b53b11f549f088679fbc1862f07be36e18455df
Olga Acosta <olgaacostab@gmail.com>
2022-07-24 01:19:27 -0500

    agregando archivo css responsive para tablet o desktop

--- src/components/register.js
+++ src/components/register.js
@@ -14,7 +14,7 @@ export const register = () => {
                     <input type="password" id="password" placeholder="Contraseña" class="input input-password" required>
                 </div>
 
-                <input type="submit" value="Create" class="primary-button login-button">
+                <input type="submit" value="Regístrate" class="primary-button login-button">
                 <p class="login-register-text">¿Ya tienes una cuenta? <a class="link" id="inicia" href="#/inicio">Inicia sesión</a></p>
             </form>
         </div>
